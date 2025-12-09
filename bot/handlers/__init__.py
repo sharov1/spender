ed230e5bb3541fd7f4643cd4dfc7160c9a929452
sharov1 <dr.sharov2011@gmail.com>
2025-12-09 17:28:52 +0400

    currency added

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,7 +1,8 @@
 from .start import router as start_router
 from .expenses import router as expenses_router
-from .echo import router as echo_router
 from .stats import router as stats_router
+from .settings import router as settings_router
+from .echo import router as echo_router
 print("Loaded handlers package")
 
 __all__ = [
